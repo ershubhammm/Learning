6ba87382a05f116eaf7ad90701ba93b65af96ede
Shubham Kushwaha <shubh.kux@gmail.com>
2024-09-18 17:12:00 +0530

    added database

--- submit.php
+++ submit.php
@@ -1,4 +1,5 @@
 <?php
+include("connection.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = $_POST['emailId'];
@@ -11,7 +12,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $gender = $_POST['genderValue'];
 
     // echo "Email = " . $email . ", Mobile Number = " . $mNumber . ", Name = " . $name . ", Father Name = " . $fName . ", DOB = " . $dob . ", Category = " . $cast . ", Languages = " . $language . ", Gender = " . $gender;
-    echo "Your data is succesfully saved!";
+
+    $insertSql = "INSERT INTO student( email, mobile_number, student_name, middle_name, dob, category, `language`, gender) 
+    VALUES ('" . $email . "', '" . $mNumber . "', '" . $name . "', '" . $fName . "','" . $dob . "', '" . $cast . "', '" . $language . "', '" . $gender . "')";
+
+    $result = mysqli_query($conn, $insertSql);
+
+    if ($result) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 
 }
 ?>
